aa69e46965540463597c73336204defe8426644c
Itamar Bachar <itamarb25@gmail.com>
2023-11-27 19:57:28 +0200

    commit for coalb

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def default_args():
     args["sim_coeff"] = 25.0
     args["std_coeff"] = 25.0
     args["cov_coeff"] = 1.0
-    args["batch_size"] = 2
+    args["batch_size"] = 64
     args["wd"] = 1e-6
     args["base_lr"] = 0.2
-    args["epochs"] = 100
+    args["epochs"] = 100  # should be 1000
     # args["lr"] = args["base_lr"] * args["batch_size"] / 256
     args["lr"] = 1.6
     return args
@@ -61,20 +61,35 @@ def train(training_dataset):
         T_mult=1,  # Factor by which T_0 is multiplied after each restart (no change)
         eta_min=0.002  # Minimum learning rate
     )
+    # Define the number of batches to accumulate gradients over
+    accumulation_steps = 4  # You can adjust this value
+
     # train the model
     for epoch in range(args['epochs']):
+        accumulated_loss = 0
         for i, batch in enumerate(train_loader):
             # zero the parameter gradients
             lars.zero_grad()
+
             # forward + backward + optimize
             with torch.cuda.amp.autocast():
                 loss = model(batch)
+            loss = loss / accumulation_steps  # Divide the loss by the accumulation steps
             scaler.scale(loss).backward()
-            scaler.step(lars)
-            scaler.update()
-            # print statistics
-            print(f'epoch: {epoch}, batch: {i}, loss: {loss.item()}')
-            # update the learning rate
+
+            accumulated_loss += loss.item()
+
+            if (i + 1) % accumulation_steps == 0:
+                # Update the model parameters after accumulating gradients
+                scaler.step(lars)
+                scaler.update()
+
+                # Print statistics
+                print(f'epoch: {epoch}, batch: {i}, accumulated loss: {accumulated_loss}')
+
+                accumulated_loss = 0  # Reset accumulated_loss
+
+            # Update the learning rate
             scheduler.step()
 
 
